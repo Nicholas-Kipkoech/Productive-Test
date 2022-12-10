cf96d7017ae79803131be74b07a0d582e66a57c9
kipkoech <nickey968@gmail.com>
2022-12-10 09:36:23 +0300

    chore(add):added banner1 and 2

--- Navigation/Home.js
+++ Navigation/Home.js
@@ -2,6 +2,8 @@ import React from "react";
 import { SafeAreaView, ScrollView, Text, View } from "react-native";
 
 import Banner1 from "../components/Banner1";
+import Banner2 from "../components/Banner2";
+import Banner3 from "../components/Banner3";
 import Carousel1 from "../components/Carousel1";
 import Carousel2 from "../components/Carousel2";
 import Carousel3 from "../components/Carousel3";
@@ -24,7 +26,9 @@ const Home = () => {
       <ScrollView>
         <Banner1 />
         <Carousel1 />
+        <Banner2 />
         <Carousel2 />
+        <Banner3 />
         <Carousel3 />
       </ScrollView>
     </SafeAreaView>
